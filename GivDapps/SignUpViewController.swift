6948f5ef5cfb71d8c8dfde4e49652e5359835adc
Guillermo-C <guillermoicolin@hotmail.com>
2017-07-19 18:57:43 -0700

    Update (GC)- Followed the UI template given for the Sign Up ViewController and attached a SignUpViewController.swift file to it. Elements of this Sign Up View are linked to its cocoa touch file but they are not functional yet.

---
+++ GivDapps/SignUpViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SignUpViewController.swift
+//  GivDapps
+//
+//  Created by Guillermo Colin on 7/19/17.
+//  Copyright © 2017 GivDapps. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBOutlet weak var firstNameTF: UITextField!
+    
+    @IBOutlet weak var lastNameTF: UITextField!
+    
+    @IBOutlet weak var emailTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBAction func addProfilePic(_ sender: UIButton) {
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
