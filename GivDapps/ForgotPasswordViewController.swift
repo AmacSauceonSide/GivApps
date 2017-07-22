2795a82cd06a59b22a6595bd0d5ae71e2541c788
Guillermo-C <guillermoicolin@hotmail.com>
2017-07-21 21:53:01 -0700

    Fix (GC) - Added buttons missing to allow the user to go back or cancel with accessing the Sign Up or Forgot Password.

--- GivDapps/ForgotPasswordViewController.swift
+++ GivDapps/ForgotPasswordViewController.swift
@@ -21,6 +21,11 @@ class ForgotPasswordViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cancel(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
 
     /*
     // MARK: - Navigation
